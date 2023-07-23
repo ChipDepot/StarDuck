d917244ab7ce04fa7bb2f93ee904e27bd054db44
dnl <danieldavid2001@gmail.com>
2023-07-23 12:14:54 -0500

    Updated component.
    Implemented ToString.
    
    Updated component to add access keywords.
    
    Implemented from_string for ComponentType
    
    Added component error InvalidComponentType
    
    Updated return type for InvalidComponentType
    
    Implemented new on Component
    
    Added Output const in Component

--- starduck/src/components/component.rs
+++ starduck/src/components/component.rs
@@ -1,13 +1,76 @@
 use std::collections::HashMap;
+use thiserror::Error;
 
 use super::output::IoTOutput;
 use super::properties::Property;
 
 #[derive(Debug, Clone)]
+pub enum ComponentType {
+    Sensor,
+    Actuator,
+}
+
+impl ComponentType {
+    pub fn from_string(s: &str) -> Result<ComponentType, ComponentError> {
+        match s.to_lowercase().as_str() {
+            "sensor" => Ok(ComponentType::Sensor),
+            "actuator" => Ok(ComponentType::Actuator),
+            s => Err(ComponentError::InvalidComponentType(s.to_string())),
+        }
+    }
+}
+
+impl ToString for ComponentType {
+    fn to_string(&self) -> String {
+        match self {
+            ComponentType::Sensor => String::from("sensor"),
+            ComponentType::Actuator => String::from("actuator"),
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum ComponentError {
+    #[error("Component `{0}` has no valid component-type")]
+    InvalidComponentType(String),
+}
+
+#[derive(Debug, Clone)]
 pub struct Component {
     pub name: String,
+    pub component_type: ComponentType,
+    pub components: HashMap<String, Box<Component>>,
     pub properties: HashMap<String, Property>,
     pub outputs: HashMap<String, IoTOutput>,
 }
 
-impl Component {}
+impl Component {
+    pub const COMPONENTS: &str = "components";
+    pub const NAME: &str = "name";
+    pub const COMPONENT_TYPE: &str = "component-type";
+    pub const OUTPUTS: &str = "outputs";
+
+    pub fn new(name: String, component_type: ComponentType) -> Component {
+        Component {
+            name,
+            component_type,
+            components: HashMap::new(),
+            properties: HashMap::new(),
+            outputs: HashMap::new(),
+        }
+    }
+}
+
+impl ToString for Component {
+    fn to_string(&self) -> String {
+        format!(
+            "name: {}\ncomponent_type: {}\ncomponents: {:?}\nproperties: {:?}\noutputs: {:?}",
+            self.name,
+            self.component_type.to_string(),
+            self.components,
+            self.properties,
+            self.outputs
+        )
+        .to_string()
+    }
+}
