a17338098bbe0daafc2aad15f93ac92833937f42
dnl <danieldavid2001@gmail.com>
2024-01-12 22:28:51 -0500

    Derive for ExecMessage

--- starduck/src/messages/exec_message.rs
+++ starduck/src/messages/exec_message.rs
@@ -1,6 +1,9 @@
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 use crate::adaptation::Directive;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 
 pub struct ExecMessage {
     pub action: Directive,
