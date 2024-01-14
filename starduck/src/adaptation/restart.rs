d03a665c657c080325fb2861bbf1b269634c30c4
dnl <danieldavid2001@gmail.com>
2024-01-14 15:23:18 -0500

    Added QueryType for RestartOrder

--- starduck/src/adaptation/restart.rs
+++ starduck/src/adaptation/restart.rs
@@ -1,13 +1,21 @@
+use std::path::PathBuf;
+
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum QueryType {
+    Http { endpoint: PathBuf },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RestartOrder {
     pub uuid: Uuid,
+    pub query_type: QueryType,
 }
 
 impl RestartOrder {
-    pub fn new(uuid: Uuid) -> Self {
-        Self { uuid }
+    pub fn new(uuid: Uuid, query_type: QueryType) -> Self {
+        Self { uuid, query_type }
     }
 }
