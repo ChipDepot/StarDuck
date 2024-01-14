94575be3083798bf2c28438bf130c9622b381295
dnl <danieldavid2001@gmail.com>
2024-01-14 16:31:02 -0500

    Added query type to lib

--- starduck/src/adaptation/mod.rs
+++ starduck/src/adaptation/mod.rs
@@ -6,6 +6,6 @@ mod restart;
 // pub use modifiers::Directive;
 pub use addition::AdditionOrder;
 pub use reconfig::{ReconfigureOrder, ReconfigureType};
-pub use restart::RestartOrder;
+pub use restart::{QueryType, RestartOrder};
 
 // pub use
